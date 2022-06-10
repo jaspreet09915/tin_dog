9a367fb4304e0e5f39e85c8e0ed59e63e17f2fdf
jaspreet09915 <jaspreetsingh09915@gmail.com>
2022-06-10 13:08:50 +0530

    import fontAwesome in aap.js in index.js favicon inported

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
-
+import { useEffect } from 'react'
+import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import('tw-elements')
+    import('tw-elements/dist/css/index.min.css')
+  }, [])
+  
   return <Component {...pageProps} />
 }
 
